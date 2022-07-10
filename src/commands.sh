e76e2f19ea1f3cadae4821ad3100909298d04d49
Thu Phan <alicebe.phan@gmail.com>
2022-07-10 17:01:45 -0500

    I am a loser! I gave up on trying SAM init and had to go back clicking around the console to make a new bucket and created a new static website.

--- src/commands.sh
+++ src/commands.sh
@@ -8,3 +8,5 @@ aws cloudformation package --s3-bucket alice-code-sam
 aws cloudformation package --s3-bucket alice-code-sam --template-file template.yaml --output-template-file gen/template-generated.yaml
 
 # so that is my transform cloudformation template
+
+# aws cloudformation package --s3-bucket alice-code-sam --template-file TheCloudResumeChallenge/src/template.yaml --output-template-file gen/template-generated.yaml
