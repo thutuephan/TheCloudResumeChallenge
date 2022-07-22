d8ff0e002b4f74437f2415e7278518b7a999f7de
Thu Phan <alicebe.phan@gmail.com>
2022-07-22 00:55:11 -0500

    to get a Python Lambda function talking to the database, I needed to switch my runtime and the code of the handler from JS to Python

--- src/app.py
+++ src/app.py
@@ -1,8 +1,24 @@
+import boto3
 import json
+
 
 print('Loading function')
 
-def lambda_handler(event, context):
-    
+def lambdaHandler(event, context):
+    return {
+        'statusCode': 200,
+        'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET',
+                'Access-Control-Allow-Headers': '*',
+                'Accept': '*/*',
+                'Content-Type': 'application/json'       
+                },
 
-    return "Hello World" 
+        'body': json.dumps({
+            'message': 'hello mother earth',
+            # 'location': ip.text.replace('\n','')
+        }),
+
+        
+    }
