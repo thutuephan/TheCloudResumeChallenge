567abaa175991fdf0fa9a5e7730308e8f1b2ba88
Thu Phan <alicebe.phan@gmail.com>
2022-07-22 22:22:28 -0500

    wrote a put lambda function to add values to the dynamodb table

---
+++ backend/put-function.py
@@ -0,0 +1,40 @@
+import boto3
+import json
+
+
+print('Loading function')
+# resource: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodb.html#DynamoDB.Table.update_item
+
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('alice-resume-cloud2')
+
+def lambda_handler(event, context):
+    response = table.update_item(
+        Key = {
+            'ID': 'visitors'
+        },
+        UpdateExpression="SET #att = #att + :val1"
+        ExpressionAttributeNames= {"#att": "Counts"}
+        ExpressionAttributeValue={":val1": 1},
+        ReturnValues="UPDATED_NEW"
+    )
+
+    numberOfVisits = response['Attributes']['visitor_count']
+    return {
+        'statusCode': 200,
+        'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET',
+                'Access-Control-Allow-Headers': '*',
+                'Accept': '*/*',
+                'Content-Type': 'application/json'       
+                },
+
+        'body': numberOfVisits
+        # json.dumps({
+        #     'message': 'hello mother earth',
+            # 'location': ip.text.replace('\n','')
+        },
+
+        
+    
