9393e09c3377d14d31a0d10e35b47b74d100cfd5
Thu Phan <alicebe.phan@gmail.com>
2022-07-26 22:47:49 -0500

    added the second test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,7 @@
+// This test is to make sure my API returns an updated value
+
 describe('My First Test', () => {
-  it('Get the visitor counts of the website', () => {
+  it('successfully loads', () => {
     cy.visit('https://alicezenmind.com')
 
     cy.contains('email').click()
@@ -7,6 +9,22 @@ describe('My First Test', () => {
     // Get an input, type into it and verify
     // that the value has been updated
     cy.get('.counter')
-
+    cy.get('#banner')
+    cy.get('#fetch')
+    
   })
 })
+
+describe('My Second Test', () => {
+  it('successfully loads', () => {
+    cy.visit('https://alicezenmind.com')
+
+    // Get an input, type into it and verify
+    // that the value has been updated
+    cy.get('.navigation')
+      .should('exist')
+      .contains('GITHUB')
+    
+  })
+})
+
